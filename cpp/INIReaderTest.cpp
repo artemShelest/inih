80dd6c15aa70fe843d31efec8f18725561bc1910
Artem Shelest <artem.e.shelest@gmail.com>
2016-04-10 19:42:17 +0300

    Helper to define section signature (names and types) in the code.

--- cpp/INIReaderTest.cpp
+++ cpp/INIReaderTest.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <assert.h>
 #include "INIReader.h"
+#include "IniSection.h"
 
 std::map<const char*, bool (*)()> test_list;
 INIReader reader("../examples/test.ini");
@@ -18,6 +19,7 @@ bool test_get_long();
 bool test_get_string();
 bool test_get_boolean();
 bool test_get_double();
+bool test_section();
 
 int main()
 {
@@ -25,6 +27,7 @@ int main()
     test_list["TestGetString"] = &test_get_string;
     test_list["TestGetBoolean"] = &test_get_boolean;
     test_list["TestGetDouble"] = &test_get_double;
+	test_list["TestSection"] = &test_section;
 
     if (reader.ParseError() < 0) {
         std::cout << "Failed to load'test.ini'\n";
@@ -115,3 +118,46 @@ bool test_get_double() {
     );
 }
 
+#define SECTION_STR_DEFAULT "QWertyuuip lkjhgfdsa"
+#define SECTION_INT_DEFAULT -23422
+#define SECTION_REAL_DEFAULT 0.36
+#define SECTION_BOOL_DEFAULT 0
+
+#define SECTION_STR_EXPECTED "@String!~@#\\"
+#define SECTION_INT_EXPECTED 101
+#define SECTION_REAL_EXPECTED 3.14159
+#define SECTION_BOOL_EXPECTED 1
+
+INIH_SECTION(confTest, testSection)
+INIH_STR_ENTRY(str, SECTION_STR_DEFAULT)
+INIH_INT_ENTRY(intValue, SECTION_INT_DEFAULT)
+INIH_REAL_ENTRY(realValue, SECTION_REAL_DEFAULT)
+INIH_BOOL_ENTRY(boolValue, SECTION_BOOL_DEFAULT)
+INIH_STR_ENTRY(strUndefined, SECTION_STR_DEFAULT)
+INIH_INT_ENTRY(intUndefined, SECTION_INT_DEFAULT)
+INIH_REAL_ENTRY(realUndefined, SECTION_REAL_DEFAULT)
+INIH_BOOL_ENTRY(boolUndefined, SECTION_BOOL_DEFAULT)
+INIH_SECTION_ENDS
+
+bool test_section()
+{
+	confTest ct(reader);
+	bool boolVal = ct.boolValue();
+	bool boolUndef = ct.boolUndefined();
+	const std::string& strVal = ct.str();
+	const std::string& strUndef = ct.strUndefined();
+	long longVal = ct.intValue();
+	long longUndef = ct.intUndefined();
+	double realVal = ct.realValue();
+	double realUndef = ct.realUndefined();
+
+	return
+		(boolVal == SECTION_BOOL_EXPECTED) &&
+		(boolUndef == SECTION_BOOL_DEFAULT) &&
+		(strVal == SECTION_STR_EXPECTED) &&
+		(strUndef == SECTION_STR_DEFAULT) &&
+		(longVal == SECTION_INT_EXPECTED) &&
+		(longUndef == SECTION_INT_DEFAULT) &&
+		(realVal == SECTION_REAL_EXPECTED) &&
+		(realUndef == SECTION_REAL_DEFAULT);
+}
---
+++ cpp/IniSection.h
@@ -0,0 +1,40 @@
+/* Macros to define section signature as a class.
+   Usage:
+   1. Define your section within a code:
+   INIH_SECTION(confTest, testSection) // at the start of a section
+   INIH_STR_ENTRY(str, SECTION_STR_DEFAULT) // define different entry types,
+   // missing entries will be returned as default values that you specify here
+   INIH_INT_ENTRY(intValue, SECTION_INT_DEFAULT)
+   INIH_REAL_ENTRY(realValue, SECTION_REAL_DEFAULT)
+   INIH_BOOL_ENTRY(boolValue, SECTION_BOOL_DEFAULT)
+   INIH_SECTION_ENDS
+   2. Use elsewhere:
+   confTest ct(reader);
+   long i = ct.intValue();
+   bool b = ct.boolValue();
+   double d = ct.realValue();
+   std::string s = ct.str();
+
+*/
+#ifndef __INISECTION_H__
+#define __INISECTION_H__
+
+#include "INIReader.h"
+
+class IniSectionBase
+{
+public:
+	IniSectionBase(const INIReader& reader) :_reader(reader) {}
+protected:
+	const INIReader& _reader;
+};
+
+#define INIH_SECTION(STRUCT_NAME, INI_NAME) class STRUCT_NAME : public IniSectionBase { protected: const char* _section = #INI_NAME; \
+	public: STRUCT_NAME(INIReader& reader) :IniSectionBase(reader){}
+#define INIH_STR_ENTRY(NAME, DEFAULT) const std::string& NAME() const { static const std::string def(DEFAULT); return _reader.Get(_section, #NAME, def); }
+#define INIH_INT_ENTRY(NAME, DEFAULT)  long NAME() const { return _reader.GetLong(_section, #NAME, DEFAULT); }
+#define INIH_REAL_ENTRY(NAME, DEFAULT) double NAME() const { return _reader.GetReal(_section, #NAME, DEFAULT); }
+#define INIH_BOOL_ENTRY(NAME, DEFAULT) bool NAME() const { return _reader.GetBoolean(_section, #NAME, DEFAULT); }
+#define INIH_SECTION_ENDS };
+
+#endif // __INISECTION_H__
